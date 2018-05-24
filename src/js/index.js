7b5c4ff0586330ec364c9977cbf14d488f8512ed
wangjiab <864789126@qq.com>
2018-05-24 18:19:06 +0800

    提交

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,6 @@ $(document).ready(function () {
         setTimeout(function () {
             $("#fullpage").fadeIn();
             $(".page-loading").hide();
-            drawDown()
         }, 800)
     })
 
@@ -146,26 +145,4 @@ function loadImages(sources, callback) {
         }
         images[src].src = sources[src];
     }
-}
-
-function drawDown() {
-    var c = document.getElementById("downCanvas"),
-        cxt = c.getContext("2d");
-    cxt.beginPath();
-    cxt.moveTo(5, 20);
-    cxt.lineTo(25, 45);
-    cxt.lineTo(45, 20);
-    cxt.lineWidth = 5;
-    cxt.strokeStyle = "white";
-    cxt.lineCap = 'round';
-    cxt.lineJoin = "round";
-    cxt.stroke();
-    cxt.moveTo(5, 5);
-    cxt.lineTo(25, 30);
-    cxt.lineTo(45, 5);
-    cxt.lineWidth = 5;
-    cxt.strokeStyle = "white";
-    cxt.lineCap = 'round';
-    cxt.lineJoin = "round";
-    cxt.stroke();
 }
