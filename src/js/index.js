c14543debab278f975d05d10c9a0818cd084d6d5
wangjiab <864789126@qq.com>
2018-05-25 17:11:53 +0800

    时间调为正常的下周一8点

--- src/js/index.js
+++ src/js/index.js
@@ -13,15 +13,15 @@ $(document).ready(function () {
     })
     onLoad();
     var OrderNum = 53;
-  
+   // console.log(timeLong())
     OrderNum = OrderNum + Math.floor(timeLong() / 3)
-     $(".order").text(OrderNum)
+    $(".order").text(OrderNum)
     // //没分钟去刷新数据
     setInterval(function () {
         //timeLong() / 3
-        OrderNum = OrderNum + Math.floor(1)
+        OrderNum = OrderNum + 1
         $(".order").text(OrderNum)
-    }, 6000)
+    }, 3600000)
 
 })
 function onLoad() {
@@ -418,23 +418,23 @@ function loadImages(sources, callback) {
 }
 //计算时间差
 function timeLong() {
-    var d1="2018-05-25 12:00:00";
-     var dateBegin = new Date(d1.replace(/-/g, "/"));//将-转化为/，使用new Date
+    var d1 = "2018-05-28 08:00:00";
+    var dateBegin = new Date(d1.replace(/-/g, "/"));//将-转化为/，使用new Date
     var dateEnd = new Date();//获取当前时间
     var dateDiff = dateEnd.getTime() - dateBegin.getTime();//时间差的毫秒数
     var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
-    var leave1=dateDiff%(24*3600*1000)    //计算天数后剩余的毫秒数
-    var hours=Math.floor(leave1/(3600*1000))//计算出小时数
+    var leave1 = dateDiff % (24 * 3600 * 1000)    //计算天数后剩余的毫秒数
+    var hours = Math.floor(leave1 / (3600 * 1000))//计算出小时数
     //计算相差分钟数
-    var leave2=leave1%(3600*1000)    //计算小时数后剩余的毫秒数
-    var minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
+    var leave2 = leave1 % (3600 * 1000)    //计算小时数后剩余的毫秒数
+    var minutes = Math.floor(leave2 / (60 * 1000))//计算相差分钟数
     //计算相差秒数
-    var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
-    var seconds=Math.round(leave3/1000)
+    var leave3 = leave2 % (60 * 1000)      //计算分钟数后剩余的毫秒数
+    var seconds = Math.round(leave3 / 1000)
     // console.log(" 相差 "+dayDiff+"天 "+hours+"小时 "+minutes+" 分钟"+seconds+" 秒")
     // console.log(dateDiff+"时间差的毫秒数",dayDiff+"计算出相差天数",leave1+"计算天数后剩余的毫秒数"
     //     ,hours+"计算出小时数",minutes+"计算相差分钟数",seconds+"计算相差秒数");
-    return hours;
+    return dayDiff*24 + hours;
 
     // alert(" 相差 " + days + "天 " + hours + "小时 " + minutes + " 分钟" + seconds + " 秒")
 
